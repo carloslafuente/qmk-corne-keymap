2fb66946cd928e1e160d0bf8818845839c46ec17
Carlos La Fuente <carlos.lafuente@escala.com>
2022-12-13 09:24:21 -0400

    QWERTY keymap created

--- keymap.c
+++ keymap.c
@@ -22,11 +22,11 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-LT(3, KC_TAB),  KC_SCLN,  KC_COMM,  KC_DOT,  KC_P,  KC_Y,                         KC_F,    KC_G,    KC_C,    KC_R,   KC_L,  KC_BSPC,
+LT(3, KC_TAB),  KC_Q,  KC_W,  KC_A,  KC_R,  KC_T,                         KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,  KC_BSPC,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-LCTL_T(KC_LCTL),  KC_A,    KC_O,    KC_E,    KC_U,    KC_I,                       KC_D,    KC_H,    KC_T,    KC_N,   KC_S,   KC_MINS,
+LCTL_T(KC_LCTL),  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                       KC_H,    KC_J,    KC_K,    KC_L,   KC_SCLN,   KC_QUOT,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LSFT,  KC_QUOT,  KC_Q,    KC_J,    KC_K,    KC_X,                        KC_B,    KC_M,    KC_W,    KC_V,   KC_Z,   RSFT_T(KC_SLASH),
+      KC_LSFT,  KC_Z,  KC_X,    KC_C,    KC_V,    KC_B,                        KC_N,    KC_M,    KC_COMM,    KC_DOT,   KC_SLASH,   KC_RSFT,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
                                           KC_LGUI,   MO(1),  KC_SPC,     KC_SPC,   LT(2, KC_ENT), KC_RALT
                                       //`--------------------------'  `--------------------------'
